8be31af6130bf9a78b2b84c2f9048dd357aa6667
SHUBHAM GUPTA <shubhamg8825@gmail.com>
2025-12-05 14:19:58 +0530

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  Loginn
+//
+//  Created by students on 14/9/47.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                VStack{
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width:300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width : CGFloat(wrongUsername))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width:300, height:50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    Button("Login"){
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width:300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen){
+                        EmptyView()
+                    }
+                }
+                        
+                    
+                
+                
+            
+        }
+        .navigationBarHidden(true)
+ 
+        }
+    
+    }
+    func authenticateUser(username: String, password: String){
+        if username.lowercased() == "mario2021"{
+            wrongUsername = 0
+            if password.lowercased() == "abc123"{
+                wrongPassword = 0
+                showingLoginScreen = true
+            }else{
+                wrongPassword = 2
+                    
+            }
+            
+            
+        }else{
+            wrongUsername = 2
+        }
+        
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginnApp.swift
+//  Loginn
+//
+//  Created by students on 14/9/47.
+//
+
+import SwiftUI
+
+@main
+struct LoginnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
